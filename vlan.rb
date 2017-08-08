f019cdcb054dafb1e3ae593e5a9eaddc91900f53
Erick Davi Morgade Pessanha <erickdavi@gmail.com>
2017-08-08 02:29:14 -0300

    Update vlan.rb

--- vlan.rb
+++ vlan.rb
@@ -1,56 +1,63 @@
 class Vlan
-	protected def conf_load
-		conf_file = 'ddhcp.conf'
-		hashfile = []
-		File.open(conf_file, 'r') do |file|
+	attr_accessor :conf_file
+
+	def initialize(conf_file)
+		@conf_file = conf_file
+
+		@hashfile = []
+		File.open(@conf_file, 'r') do |file|
 			while line = file.gets
 				colun = line.split(':')
 				hashline = {vlan: colun[0], ip: colun[1], status: colun[2].chomp}
-				hashfile.push(hashline)
+				@hashfile.push(hashline)
 			end
 		end
-		return hashline
-	end	
-	protected def test_ip(ip)
-		indx = self.conf_load.index do |hashfile|
-			hashfile[:ip] == ip
-		end
-		if indx == nil
-			out = [false,nil,nil]
-		else
-			hashline = self.conf_load[indx]
-			out = [true,hashline,indx]
-		end			
 	end
-	protected def format_cachefile(hashfile)
+	
+	def format_cachefile
 		cachefile = ""
-		hashfile.each do |hashline|
+		@hashfile.each do |hashline|
 			cachefile = cachefile + "#{hashline[:vlan]}:#{hashline[:ip]}:#{hashline[:status]}\n"
 		end			
 		return cachefile
-	end	
-
-	protected def get_ip(ip)
-		hashfile = self.conf_load
-		mtrx_status = test_ip(ip)
-		hashline = mtrx_status[1]
-		indx = mtrx_status[2]
-		if mtrx_status[0]
-			if hashline[:status] == 'free'
-				hashline[:status] = 'busy'
-				hashfile[indx] = hashline
-				cachefile = self.format_cachefile(hashfile)	
-				conffile = File.new('ddhcp.conf','w')
-				conffile.puts(cachefile)
-				conffile.close
-				#log_entrie = "#{email} are using #{ip} "
-			elsif hashline[:status] == 'busy'
-				out = 'No change has done'
-			else 
-				out = 'Configuration file error'
-			end
-		else
-			out = 'Ip does not exist in configuration file'		
+	end
+	
+	def test_ip(ip)
+		indx = @hashfile.index do |hashline|
+			hashline[:ip] == ip
 		end
-	end		
+		if indx != nil
+			out = {exists: true, status: @hashfile[indx][:status], indx: indx}
+		else
+			out = {exists: false, status: nil,indx: nil}
+		end			
+	end
+
+	def change_status(ip)
+		ip_data = self.test_ip(ip)
+		if ip_data[:exists] and ip_data[:status] == 'busy' or ip_data[:status] == 'free'
+			if ip_data[:status] == 'busy'
+				
+				#altera a linha para free
+
+			elsif ip_data[:status] == 'free'
+
+				#altera a linha para busy
+
+			end
+			#Com sorte reescreve o arquivo com a nova conf
+			cache = format_cachefile
+			file = File.new(@conf_file)
+			file.puts(cache)
+
+		else
+			out = 'Configuration file error - BAD STATUS'
+		end
+
+	end	
+	def get_hashfile
+		return @hashfile
+	end
 end
+
+vl = Vlan.new('ddhcp.conf')
