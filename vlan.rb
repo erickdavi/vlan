a3bd2d4a6694c587c92d1c33803a37b1a446ca7f
erickdavi <erickdavi@gmail.com>
2017-08-31 15:52:30 -0300

    Aplicada alteração em change_status e implementada a função newip

--- vlan.rb
+++ vlan.rb
@@ -1,3 +1,4 @@
+
 #!/usr/local/bin/ruby
 class Vlan
 	attr_accessor :conf_file
@@ -32,8 +33,15 @@ class Vlan
 	protected def change_status(ip)
 		ip_data = self.test_ip(ip)
 		if ip_data[:exists] and ip_data[:status] == 'busy' or ip_data[:status] == 'free'
-			if ip_data[:status] == 'busy'				
-				@hashfile[ip_data[:indx]][:status] = 'free'				
+			if ip_data[:status] == 'busy'
+				puts "This ip address is already busy"
+				puts "Do you want to use it anyway?"
+				puts "y/n"
+				response = gets
+				if response.chomp.capitalize == "Y"
+					puts response
+					@hashfile[ip_data[:indx]][:status] = 'free'
+				end									
 			elsif ip_data[:status] == 'free'
 				@hashfile[ip_data[:indx]][:status] = 'busy'
 			end
@@ -44,16 +52,21 @@ class Vlan
 		end
 	end	
 
-	def newip(vlan, ip)
-		if self.test_ip(ip)
+	protected def newip(vlan, ip)
+		ip_query = self.test_ip(ip)
+		if !(ip_query[:exists])
 			hashline = {vlan: vlan, ip: ip, status: 'free'}
 			@hashfile.push(hashline)
 			puts @hashfile
-			#File.new(@conf_file,'w').puts(format_cachefile)	
+			File.new(@conf_file,'w').puts(format_cachefile)
+			out = true
+		else
+			out = false	
 		end
 	end
 end
 
-#cmd = Vlan.new("ddhcp.conf")
+cmd = Vlan.new("ddhcp.conf")
 
-#cmd.newip("erick", "10.0.0.1")
+#cmd.newip("erick", "10.0.0.2")
+cmd.change_status("10.1.10.22")
